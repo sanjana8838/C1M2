d5a468d75cbf283ebe4b59d8e85282b2ff45988d
Sanjana Ramesh <sanjana8838@gmail.com>
2020-05-20 02:53:39 +0530

    code added to .mk file

--- src/sources.mk
+++ src/sources.mk
@@ -10,8 +10,14 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
+
+SOURCES =	\
+		./main.c \
+		./memory.c 
 
 # Add your include paths to this variable
-INCLUDES = 
+INCLUDES =  \
+	   -I./CMSIS \
+	   -I./common \
+	   -I./msp432
 
